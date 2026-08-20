3ff6cf26c339ff543b44c4958303f79ef924958c
agency.ai <samarth.more.work@gmail.com>
2026-08-20 12:38:54 +0530

    logo and navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+
+function App() {
+  const [theme,setTheme] = useState('light')
+  return (
+
+    <div className='dark:bg-black relative'><Navbar theme={theme} setTheme={setTheme}/></div>
+  )
+}
+
+export default App
